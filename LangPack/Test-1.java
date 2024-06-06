14c538b3b6a485b0d47976b276ff3d3e3c423c33
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-06-06 18:28:12 +0530

    tsT

---
+++ LangPack/Test-1.java
@@ -0,0 +1,11 @@
+class Test{
+    public void m1(){
+        System.out.println("Test class  m1 method");
+    }
+    public static void main(String[] args) {
+       Test t1=new Test();
+       t1.m1();
+       System.out.println(t1);
+    }
+
+}
---
+++ LangPack/Test-2.java
@@ -0,0 +1,12 @@
+public class Test {
+    public String toString(){
+        return "Rajni Kanth";
+    }
+public static void main(String[] args) {
+    //String[] enames = {"Rahul","Sonia"};
+    Test t1=new Test();
+    System.out.println(t1);
+    System.out.println(t1.toString());
+    //System.out.println(enames);
+}    
+}
